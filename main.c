4abcaa0e7d781fdc224264d7437addf98a460a28
DaiQing1995 <dq_199510@163.com>
2019-11-18 15:07:07 +0800

    save2DB function head added

--- main.c
+++ main.c
@@ -1,39 +1,49 @@
 #include<cspider/spider.h>
+#include<cspider/cJSON.h>
 #include <locale.h>
 
-void p(cspider_t *cspider, char *d, char *url, void *user_data) {
-  char *get[10];
-  int size = xpath(d, "//a/@href", get, 10);
-//  int size = regexAll("http:\\/\\/(.*?)\\.html", d, get, 3, REGEX_ALL);
+void save2DB(cJSON *data){
 
-//  joinall(url,get,size);
-//  addUrls(cspider, get, size);
-  saveStrings(cspider, (void**)get, size, LOCK);
-  freeStrings(get, size);
+}
+
+void p(cspider_t *cspider, char *d, char *url, void *user_data) {
+    printf("%s crawled\n", url);
+    cJSON *json = cJSON_Parse(d);
+    cJSON *items = cJSON_GetObjectItem(json, "summaries");
+    int datasum = cJSON_GetArraySize(items);
+    for (int i = 0; i < datasum; i ++){
+      cJSON *item = cJSON_GetArrayItem(items, i);
+      printf("%s\n", cJSON_Print(item));
+      save2DB(item);
+    }
 }
 
 void s(void *str, void *user_data) {
-  char *get = (char*)str;
-  FILE *file = (FILE*)user_data;
-  fprintf(file, "%s\n", get);
-  return;
+    char *get = (char*)str;
+    FILE *file = (FILE*)user_data;
+    printf("[check point2]hahahah, s running\n");
+    printf("get:%s\n", get);
+    fprintf(file, "%s\n", get);
+    return;
 }
 
 int main() {
-  cspider_t *spider = init_cspider();
-  char *agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:42.0) Gecko/20100101 Firefox/42.0";
-  setlocale(LC_ALL,"");
+    cspider_t *spider = init_cspider();
+    char *agent = "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0";
+    char *root_page = "https://hub.docker.com/api/content/v1/products/search?image_filter=official&page_size=160&q=&type=image";
 
-  cs_setopt_url(spider, "https://hub.docker.com/search?q=&type=image&image_filter=official&page=1");
+    setlocale(LC_ALL,"");
+    
+    cs_setopt_url(spider, root_page);
 
-  cs_setopt_useragent(spider, agent);
-  //指向自定义的解析函数，和数据持久化函数
-  cs_setopt_process(spider, p, NULL);
-  cs_setopt_save(spider, s, stdout);
-  //设置抓取线程数量，和数据持久化的线程数量
-  cs_setopt_threadnum(spider, DOWNLOAD, 2);
-  cs_setopt_threadnum(spider, SAVE, 2);
-  cs_setopt_logfile(spider, stdout);
+    cs_setopt_useragent(spider, agent);
+    //指向自定义的解析函数，和数据持久化函数
+    cs_setopt_process(spider, p, NULL);
+    cs_setopt_save(spider, s, stdout);
+    //设置抓取线程数量，和数据持久化的线程数量
+    cs_setopt_threadnum(spider, DOWNLOAD, 1);
+    cs_setopt_threadnum(spider, SAVE, 1);
+    cs_setopt_logfile(spider, stdout);
 
-  return cs_run(spider);
+    return cs_run(spider);
 }
